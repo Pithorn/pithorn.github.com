d8b82ff29cf1fccd16d93508dcaf4efff6e70a04
Jingbei Li <i@jingbei.li>
2016-09-17 17:30:05 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 hexo generate
 git checkout public
-rm -r css fancybox js index.html db.json
 mv public/* .
-git push origin public
 git add .
 git commit -m "[$(date)] auto-build"
+git push origin public
